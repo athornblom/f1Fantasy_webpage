ae59a1d59f5bbac952747439499cda8b2f53bfb9
athornblom <adam.thornblom@gmail.com>
2022-04-25 19:30:02 +0200

    Update PodiumCard.tsx

--- src/components/standings/PodiumCard.tsx
+++ src/components/standings/PodiumCard.tsx
@@ -20,7 +20,7 @@ const PodiumCard: NextPage<Props> = ({ winner, team, owner, image }) => {
       <div>
         <Image
           layout="responsive"
-          src={image ? image : '/F12022TemplateCar.png'}
+          src={image ? image : '/f12022TemplateCar.png'}
           width="800"
           height="220"
           alt="Picture of winners car"
