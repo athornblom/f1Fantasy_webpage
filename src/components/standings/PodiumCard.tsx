748ecb115c28fb3881deeb30f21ae519aba3fce9
athornblom <adam.thornblom@gmail.com>
2022-04-25 20:10:51 +0200

    Default image works now

--- src/components/standings/PodiumCard.tsx
+++ src/components/standings/PodiumCard.tsx
@@ -20,7 +20,7 @@ const PodiumCard: NextPage<Props> = ({ winner, team, owner, image }) => {
       <div>
         <Image
           layout="responsive"
-          src="templateCar.png"
+          src={image ? image : '/templateCar.png'}
           width="800"
           height="220"
           alt="Picture of winners car"
